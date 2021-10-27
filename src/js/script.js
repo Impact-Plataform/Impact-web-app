71fc301d51448aedf868b7d82d22bd5c054be069
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-10-27 19:45:21 -0300

    Adicionando validação de formulário

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,36 @@
-function validar (){
+let email = document.querySelector("#email");
+let entrar = document.querySelector("#entrar");
+let password = document.querySelector("#password");
+let output = document.createElement("span");
+output.classList.add("output");
 
-    var nome= form.name.value;
-    var senha= form.password.value;
 
-    if( nome=== "" || nome.indexOf("@") == -1 ){
-        alert("Preencha o campo User com um E-mail valido");
-        form.name.focus();
-        return false;
-    }
 
-    if( senha=== ""){
-        alert("Preencha o campo Pasword");
-        form.password.focus();
-        return false;
-    }
+function check() {
+  if (email.value.indexOf("@") == -1) {
+    output.innerHTML = "Preencha os campos corretamentes";
+    email.parentNode.insertBefore(output, entrar);
+    email.classList.add("error");
+    return false;
+  } else {
+    email.classList.remove("error");
+    output.remove();
+  }
+  if (password.value == "") {
+    output.innerHTML = "Preencha os campos corretamente";
+    password.parentNode.insertBefore(output, entrar);
+    password.classList.add("error");
+    return false;
+  } else {
+    password.classList.remove("error");
+    output.remove();
+  }
+  return true;
+}
 
-};
+document.querySelectorAll('input').forEach(input =>input.addEventListener("blur", () => check()));
+
+entrar.addEventListener("click", e => {
+    e.preventDefault();
+    check();
+});
