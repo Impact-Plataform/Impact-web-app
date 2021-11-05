f2be8d69287cc9a1b411c613cea97811b0af09a2
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-11-05 02:04:11 -0300

    Aplicando regras de Autenticação e Logout

--- src/js/dashboard.js
+++
@@ -1,9 +0,0 @@
-/* document.onreadystatechange = () => {
-  if (window.sessionStorage.getItem('token') === null) {
-    window.location.href = 'login.html';
-  }
-}
-
-let admin = document.querySelector('#admin');
-
-admin.innerHTML = window.sessionStorage.getItem('user'); */
--- src/js/login.js
+++ src/js/login.js
@@ -4,15 +4,18 @@ let password = document.querySelector("#password");
 let output = document.createElement("span");
 output.classList.add("output");
 
-function check() {
-  if (email.value.indexOf("@") == -1 && email.value.indexOf('.') == -1) {
-    output.innerHTML = "Preencha os campos corretamentes";
-    email.parentNode.insertBefore(output, entrar);
-    email.classList.add("error");
-    return false;
-  } else {
-    email.classList.remove("error");
-    output.remove();
+function check(e) {
+  if (e.target.name === "email") {
+    if (email.value.indexOf("@") == -1 && email.value.indexOf('.') == -1) {
+      output.innerHTML = "Preencha os campos corretamentes";
+      email.parentNode.insertBefore(output, entrar);
+      email.classList.add("error");
+      return false;
+    } else {
+      email.classList.remove("error");
+      output.remove();
+    }
+    return true;
   }
   if (password.value == "") {
     output.innerHTML = "Preencha os campos corretamente";
@@ -26,11 +29,13 @@ function check() {
   return true;
 }
 
-document.querySelectorAll('input').forEach(input => input.addEventListener("blur", () => check()));
+document.querySelectorAll('input').forEach(input => input.addEventListener("blur", e => check(e)));
 
 entrar.addEventListener("click", async e => {
   e.preventDefault();
-  check();
+  if(!check(e)){
+    return;
+  }
 
   const credenciais = {
     email: email.value,
@@ -49,8 +54,8 @@ entrar.addEventListener("click", async e => {
 
   if (ret.status === 200) { 
     window.sessionStorage.setItem('token', retJson.token)
-    window.sessionStorage.setItem('user', retJson.user.name)
-    window.sessionStorage.setItem('userType', retJson.user.type)
+    window.sessionStorage.setItem('user', retJson.user)
+    window.sessionStorage.setItem('admin', retJson.admin)
     window.location.href = './src/pages/dashboard.html'
   } else { 
     output.innerHTML = retJson.message
