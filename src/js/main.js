3069a2ee0ade2f8a5d26a71684238da4c1b8da2d
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-11-05 04:12:58 -0300

    Estilizando navbar

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ if (!window.sessionStorage.getItem('token')) {
   window.location.href = "/Impact-web-app";
 }
 window.onload = () => {
-  const admin = document.querySelector('.admin').innerHTML = window.sessionStorage.getItem('user')
+  const admin = document.querySelector('#admin').innerHTML = window.sessionStorage.getItem('user')
   document.querySelector('#logout').addEventListener('click', () => {
     sessionStorage.removeItem('token');
     window.location.href = '/Impact-web-app';
