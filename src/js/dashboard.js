d069780c687034f207ea99a7576153679f90a8b1
Alex Silva <alexsaraujo92@gmail.com>
2021-11-08 23:00:23 -0300

    Alunos info gerais

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -7,24 +7,30 @@
       'authorization': 'Bearer' + ' ' + sessionStorage.getItem('token')
     }
   })
+
   const retJson = await ret.json()
   const students = retJson.students
+
   window.sessionStorage.removeItem('student')
   window.sessionStorage.removeItem('student_id')
+
   students.forEach(student => {
     const cell = document.createElement('tr')
     cell.innerHTML = `
-    <td><a id="${students.student_id}" href="./student.html">${student.name}</a></td>
+    <td><a id="${students.student_id}" href="FormPreenchido.html?${student.student_id}">${student.name}</a></td>
     <td>${student.birthdate}</td>
     <td>${student.contacts.phone}</td>`
+
     document.querySelector('tbody').appendChild(cell)
     document.querySelector(`#${students.student_id}`).addEventListener('click', () => {
 
       window.sessionStorage.setItem('student', student)
       window.sessionStorage.setItem('student_id', student.student_id)
+
     })
   })
 })()
+
 
 
 
---
+++ src/js/formPreenchido.js
@@ -0,0 +1,12 @@
+
+
+
+console.log(document.URL)
+
+tamanho = document.URL.length
+
+id = document.URL[tamanho - 1]
+
+console.log(id)
+
+
