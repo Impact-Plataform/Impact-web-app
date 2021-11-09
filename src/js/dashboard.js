789d3def29fc3432ebe3a99f3961c7bf436c514f
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-11-09 01:44:18 -0300

    Editando tabela estudantes

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -17,6 +17,7 @@
   students.forEach(student => {
     const cell = document.createElement('tr')
     cell.innerHTML = `
+    <td><a id="${students.student_id}" href="FormPreenchido.html?${student.student_id}"><img src="../assets/edit.png" alt="Editar Cadastro"></a></td>
     <td><a id="${students.student_id}" href="FormPreenchido.html?${student.student_id}">${student.name}</a></td>
     <td>${student.birthdate}</td>
     <td>${student.contacts.phone}</td>`
