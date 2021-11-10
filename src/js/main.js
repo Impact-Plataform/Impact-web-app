633650d249f0947db6d685ae268c0dc513d14c9a
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-11-09 22:23:53 -0300

    Alterando header e navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 
-if (!window.sessionStorage.getItem('token')) {
-  window.location.href = "/Impact-web-app";
-}
-window.onload = () => {
-  document.querySelector('#admin').innerHTML = window.sessionStorage.getItem('user')
-  if (sessionStorage.getItem('admin') !== 'true') {
-    document.querySelector('.add').remove()
-  }
-  document.querySelector('#logout').addEventListener('click', () => {
-    sessionStorage.removeItem('token');
-    sessionStorage.removeItem('user');
-    sessionStorage.removeItem('admin');
-    window.location.href = '/Impact-web-app';
-  })
-}
+// if (!window.sessionStorage.getItem('token')) {
+//   window.location.href = "/Impact-web-app";
+// }
+// window.onload = () => {
+//   document.querySelector('#admin').innerHTML = window.sessionStorage.getItem('user')
+//   if (sessionStorage.getItem('admin') !== 'true') {
+//     document.querySelector('#add').remove()
+//   }
+//   document.querySelector('#logout').addEventListener('click', () => {
+//     sessionStorage.removeItem('token');
+//     sessionStorage.removeItem('user');
+//     sessionStorage.removeItem('admin');
+//     window.location.href = '/Impact-web-app';
+//   })
+// }
