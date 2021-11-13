26750019565ac81863fdd763332cf338f713bc44
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-11-12 22:43:17 -0300

    Consertando bug na adição de estudantes

--- src/js/adicionar-usuario.js
+++ src/js/adicionar-usuario.js
@@ -1,23 +1,34 @@
 document.querySelector('#formUser').addEventListener('submit', async e => {
   e.preventDefault();
   const user = {
-    nome: document.querySelector('#nome').value,
-    sobrenome: document.querySelector('#sobrenome').value,
+    name: document.querySelector('#nome').value,
+    surname: document.querySelector('#sobrenome').value,
     email: document.querySelector('#email').value,
-    senha: document.querySelector('#password').value,
+    password: document.querySelector('#password').value,
     admin: document.querySelector('#adm').checked
-
   }
-  console.log(user);
-  let ret = await fetch('https://impact-app.herokuapp.com/user/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        'authorization': 'Bearer' + ' ' + sessionStorage.getItem('token')
-      },
-    body: JSON.stringify(user),
-  })
-  let data = await ret.json();
-  console.log(data);
-
+  try {
+    let ret = await fetch('https://impact-app.herokuapp.com/user/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+          'authorization': 'Bearer' + ' ' + sessionStorage.getItem('token')
+        },
+      body: JSON.stringify(user),
+    })
+    let retJson = await ret.json();
+    if (ret.status == 201) {
+      output.classList.add('success');
+      output.style.color = 'green';
+      output.innerText = retJson.message;
+      document.querySelector('#formUser').reset();
+    } else {
+      output.style.color = 'red';
+      output.innerText = retJson.error;
+    }
+  } catch (error) {
+    output.innerText = error.message;
+    output.style.color = 'red';
+    
+  }
 })
